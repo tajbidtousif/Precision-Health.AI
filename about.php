cbfc3c5e732e05b41292dad07f348f9caec4683a
tajbidtousif <tajbidtousif@gmail.com>
2023-08-16 01:03:11 +0600

    About page

--- about.php
+++ about.php
@@ -2,8 +2,8 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <title>About Us - Precision Health</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
