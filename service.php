df277e1150487d37617afb3cf47ecace381eff29
Shah Tajbid Tousif <tajbidtousif@gmail.com>
2024-02-29 00:29:17 +0600

    Service page under dev

--- service.php
+++ service.php
@@ -212,7 +212,7 @@ if (isset($_POST['logout'])) {
         <?php else: ?>
           <!-- If not logged in, display login button -->
           <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            
             <button class="btn btn-outline-success" type="button" onclick="redirectToIndex()">Login</button>
           </form>
         <?php endif; ?>
